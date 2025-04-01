0df7b9ed7841cd9b8a96eda91617f5cfab9b1e8f
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-01 22:27:25 +0200

    testwithdouble++

---
+++ README.md
@@ -0,0 +1,12 @@
+# A (test) Version of a PostFix Calculator
+
+## (test) Idea
+
+>Mission: reading a String and interpreting it as a Post Fix Calculation and then solving it and 
+>returning the solution (as a String or a Integer or Double, doesn't matter for now)
+>
+<br>
+
+>Idea: using char arrays with a loop and checking if the char is a operator like: (+,-,*,/) and then with the operator calculating the last two digits.
+>
+>
--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,10 +1,49 @@
 package at.fhj.msd;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String test = "2 3 / 4 5 + +";
+        double add = 0;
+
+        String[] ctest = test.split(" ");
+        double[] bof = new double[ctest.length];
+        ArrayList<Double> addi = new ArrayList<>();
+
+        for (int i = 0; i < ctest.length; i++) {
+
+if (!(ctest[i].equals("+") && ctest[i-1].equals("+")))
+{
+    switch (ctest[i]) {
+        case "+" -> {
+            add = bof[i-2] + bof[i-1];
+            addi.add(add);
+        }
+        case "-" -> {
+            add = bof[i-2] - bof[i-1];
+            addi.add(add);
+        }
+        case "*" -> {
+            add = bof[i-2] * bof[i-1];
+            addi.add(add);
+        }
+        case "/" -> {
+            add = bof[i-2] / bof[i-1];
+            addi.add(add);
+        }
+        default -> bof[i] = Double.parseDouble(ctest[i]);
     }
+}
+else{
+    add = addi.get(0) + addi.get(1);
+}
+        }
+        System.out.println(add);
+        
+    }
+
 }
