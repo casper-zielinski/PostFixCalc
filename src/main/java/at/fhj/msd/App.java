7d880f345ec4c67d45fda917c22d487e8646586e
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-02 10:15:18 +0200

    test1

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -7,43 +7,68 @@ import java.util.ArrayList;
  */
 public class App {
     public static void main(String[] args) {
-        String test = "2 3 / 4 5 + +";
+        String test = "1 2 * 1 3 * * ";
         double add = 0;
+        double addend = 0;
 
         String[] ctest = test.split(" ");
         double[] bof = new double[ctest.length];
         ArrayList<Double> addi = new ArrayList<>();
+        String[] signs = {"+", "-", "*", "/"};
+        int count = -1;
 
         for (int i = 0; i < ctest.length; i++) {
 
-if (!(ctest[i].equals("+") && ctest[i-1].equals("+")))
-{
-    switch (ctest[i]) {
-        case "+" -> {
-            add = bof[i-2] + bof[i-1];
-            addi.add(add);
-        }
-        case "-" -> {
-            add = bof[i-2] - bof[i-1];
-            addi.add(add);
-        }
-        case "*" -> {
-            add = bof[i-2] * bof[i-1];
-            addi.add(add);
-        }
-        case "/" -> {
-            add = bof[i-2] / bof[i-1];
-            addi.add(add);
-        }
-        default -> bof[i] = Double.parseDouble(ctest[i]);
-    }
-}
-else{
-    add = addi.get(0) + addi.get(1);
-}
-        }
-        System.out.println(add);
-        
-    }
+            if (!((ctest[i].contains(signs[0]) || ctest[i].contains(signs[1])
+            || ctest[i].contains(signs[2]) || ctest[i].contains(signs[3]))
 
-}
+            && (ctest[i - 1].contains(signs[0]) || ctest[i - 1].contains(signs[1])
+            || ctest[i - 1].contains(signs[2]) || ctest[i - 1].contains(signs[3]))))
+
+            {
+                switch (ctest[i]) {
+                    case "+" -> {
+                        add = bof[i-2] + bof[i-1];
+                        addi.add(add);
+                        ++count;
+                    }
+                    case "-" -> {
+                        add = bof[i-2] - bof[i-1];
+                        addi.add(add);
+                        ++count;
+                    }
+                    case "*" -> {
+                        add = bof[i-2] * bof[i-1];
+                        addi.add(add);
+                        ++count;
+                    }
+                    case "/" -> {
+                        add = bof[i-2] / bof[i-1];
+                        addi.add(add);
+                        ++count;
+                    }
+                    default -> bof[i] = Double.parseDouble(ctest[i]);
+                }
+            }
+            else if (ctest[i].equals("+"))
+            {
+                addend = addi.get(count-1) + addi.get(count);
+            }
+            else if (ctest[i].equals("-"))
+            {
+                addend = addi.get(count-1) - addi.get(count);
+            }
+            else if (ctest[i].equals("*"))
+            {
+                addend = addi.get(count-1) * addi.get(count);
+            }
+            else if (ctest[i].equals("/"))
+            {
+                addend = addi.get(count-1) / addi.get(count);
+            }
+                    }
+                    
+                    System.out.println(addend);
+                }
+
+            }
