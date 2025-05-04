e0ea92c867d77969a57c8b661b29eb15652f837c
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-05-04 23:27:26 +0200

    Implemented PostFix Calculator.

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,12 +1,19 @@
 package at.fhj.msd;
 
-import java.util.ArrayList;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+
+        try {
+
+            PostFixCalculator calc = new PostFixCalculator();
+            calc.calculatePostFix("2 3 1 * + 9 - 4 5 * +");
+        } catch (Exception e) {
+            System.out.println("fehler");
+        }
+
         
             }
         }
--- src/main/java/at/fhj/msd/PostFixCalculator.java
+++ src/main/java/at/fhj/msd/PostFixCalculator.java
@@ -1,26 +1,70 @@
 package at.fhj.msd;
 
 public class PostFixCalculator {
-      private String expression;
 
-      PostFixCalculator(String expression) {
-          this.expression = expression;
-      }
+    private String expression;
 
-      public String calculatePostFix(String expression) {
-            // Erstelle eine Instanz der LinkedList-Klasse
-            MySinglyLinkedList stack = new MySinglyLinkedList(); // Stack für die Berechnung
-            String[] tokens = expression.split(" "); // Ausdruck in Tokens zerlegen
+    public PostFixCalculator() {
+    }
 
+    public PostFixCalculator(String expression) {
+        this.expression = expression;
+    }
+
+    public void calculatePostFix(String expression) {
+        // Erstelle eine Instanz der LinkedList-Klasse
+        MySinglyLinkedList<String> stack = new MySinglyLinkedList<>(); // Stack für die Berechnung
+        String[] tokens = expression.split(" "); // Ausdruck in Tokens zerlegen
+
+        // Berechne den Wert des Postfix-Ausdrucks
+        boolean status = true;
+        boolean status2 = true;
+        String operator = "";
+        int count = 0;
+
+        while (status) {
+            for (;count < tokens.length; count++) {
+                if (tokens[count].equals("+") || tokens[count].equals("-") || tokens[count].equals("*") || tokens[count].equals("/")) {
+                    operator = tokens[count];
+                    status2 = false;
+                    count++;
+                    break;
+                }
+                stack.addFirst(tokens[count]);
+            }
+               
+                int result = 0;
+
+                if (status2 == false) {
+                    int number2 = Integer.parseInt(stack.removeFist());
+                    int number1 = Integer.parseInt(stack.removeFist());
+
+                    if (operator.equals("+")) {
+                        result = number1 + number2;
+                    }
+                    if (operator.equals("-")) {
+                        result = number1 - number2;
+                    }
+                    if (operator.equals("*")) {
+                        result = number1 * number2;
+                    }
+                    if (operator.equals("/")) {
+                        result = number1 / number2;
+                    }
+
+                    String resultString = Integer.toString(result);
+
+                    stack.addFirst(resultString);
+                    status2 = true;
+                }
+
+               if (count == tokens.length) {
+                status = false;
+               }
             
+        }
+        stack.printList();
 
-            // Berechne den Wert des Postfix-Ausdrucks
+    }
 
-
-            return "";
-      }
-
-      
-      }
-
-
+}
