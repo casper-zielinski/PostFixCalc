180d05b2085e8ccd8b1d0b143cdd936fbe2a2c1e
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-08 12:55:10 +0200

    Updated Error Handling with JavaFX and Design

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -10,19 +10,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-@Override
+      @Override
       public void start(Stage primaryStage) {
         
           try {
-              Parent root = FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
-              Scene scene = new Scene(root);
-              scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+              Parent root = FXMLLoader.load(getClass().getResource("/MainScene.fxml")); // Load the FXML file
+              Scene scene = new Scene(root);    // Create a new scene with the loaded root node
+              scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm()); // Add the CSS stylesheet to the scene
 
 
 
               primaryStage.setTitle("Post Fix Calculator");
               primaryStage.setScene(scene);
-              primaryStage.show();
+              primaryStage.show(); // Show the primary stage
               } 
               catch (IOException ex) {
                   System.out.println("IOException Error, File either not found/corrupted etc.");
--- src/main/java/at/fhj/msd/MainSceneController.java
+++ src/main/java/at/fhj/msd/MainSceneController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class MainSceneController {
 
@@ -29,10 +30,15 @@ public class MainSceneController {
 
     @FXML
     private Label lb_mathprint;
-    
-    @FXML
-    private Label lb_info;
 
+    @FXML
+    private Pane background;
+
+
+    /**
+     * Calulate the postfix expression and show the result in the text area
+     * @param event
+     */
     @FXML
     void btn_onClick_calc(ActionEvent event) {
       try {
@@ -40,32 +46,54 @@ public class MainSceneController {
             String result = calc.calculatePostFix(tf_postfix.getText());
             tf_calculated.setText(result);
           
-      } catch (Exception e) {
-            tf_calculated.setText("Error\n \nSyntax");
+      } catch (ArithmeticException e) {
+            tf_calculated.setText("Error\n \nDivision by 0");
+      } catch (IllegalArgumentException e) {
+            tf_calculated.setText("Error\n \nInvalid character in expression");      
       }
-      
-    }
+      catch (Exception e) {
+                  tf_calculated.setText("Error\n \nSyntax");      
+      }
+   }
 
+    /**
+     * Convert the infix expression to postfix and show the result in the text area
+     * @param event
+     */
     @FXML
     void btn_onClick_in_to_post(ActionEvent event) {
       try {
             PostFixCalculator calc = new PostFixCalculator();
             String result = calc.convertInfixToPostfix(tf_postfix.getText());
             tf_calculated.setText(result);
-      } catch (Exception e) {
-            tf_calculated.setText("Error\n \nSyntax");
+      }  catch (ArithmeticException e) {
+            tf_calculated.setText("Error\n \nDivision by 0");
+      } catch (IllegalArgumentException e) {
+            tf_calculated.setText("Error\n \nInvalid character in expression");      
+      }
+      catch (Exception e) {
+                  tf_calculated.setText("Error\n \nSyntax");      
       }
     }
 
+      /**
+      * Convert the postfix expression to infix and show the result in the text area
+      * @param event
+      */
     @FXML
     void btn_onClick_post_to_in(ActionEvent event) {
       try {
             PostFixCalculator calc = new PostFixCalculator();
             String result = calc.convertPostfixToInfix(tf_postfix.getText());
             tf_calculated.setText(result);
-      } catch (Exception e) {
-            tf_calculated.setText("Error\n \nSyntax");
+      }  catch (ArithmeticException e) {
+            tf_calculated.setText("Error\n \nDivision by 0");
+      } catch (IllegalArgumentException e) {
+            tf_calculated.setText("Error\n \nInvalid character in expression");      
       }
-    }
+      catch (Exception e) {
+                  tf_calculated.setText("Error\n \nSyntax");      
+      }
 
+    }
 }
